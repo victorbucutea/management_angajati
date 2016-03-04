709f8c0d7076d50c0a3235b7de88ec7c30e91b44
TOADER Laurentiu-Victor <279766@dom1.ad.sys>
2016-03-04 17:54:08 +0200

    v0.1 040316 1754

--- Management-Angajati/src/main/java/com/victortoader/angajati/client/EmployeeController.java
+++ Management-Angajati/src/main/java/com/victortoader/angajati/client/EmployeeController.java
@@ -4,14 +4,15 @@ import com.victortoader.angajati.server.EmployeeService;
 
 /**
  * Created by 286868 on 3/3/2016.
- *
+ * <p/>
  * toate actiunile care se executa din Interfata.
- *
+ * <p/>
  * Tot ce este comportament sau actiune se pune in 'Controller'
- *
- *
  */
 public class EmployeeController {
+
+
+
 
     private EmployeeView view;
 
@@ -20,21 +21,23 @@ public class EmployeeController {
     private EmployeeService service;
 
 
-    public void showPicture(){
+    public void showPicture() {
         // comanda view-ului sa afiseze poze
     }
 
 
-    public void refresh(){
+    public void refresh() {
 
     }
 
-    public void exportReport(){
+    public void exportReport() {
 
+    }
+
+    public void importReport() {
     }
 
     // write all the methods that
-
 
 
 }
--- Management-Angajati/src/main/java/com/victortoader/angajati/client/EmployeeView.java
+++ Management-Angajati/src/main/java/com/victortoader/angajati/client/EmployeeView.java
@@ -6,17 +6,41 @@ import java.awt.event.ActionListener;
 
 /**
  * Created by 286868 on 3/3/2016.
- *
+ * <p/>
  * Tot ce tine de 'display'. Layout, butoane, tabele reactie la interactiune strict vizuala.
  * Interactiunea cu server-ul sau interactiune care genereaza alte efecte este deja comportament si ar
  * trebui sa fie in controller.
  */
 public class EmployeeView {
 
+    JTextField TxtNume;
+    JTextField TxtPrenume;
+    JTextField TxtSalariu;
+    JTextField TxtAdresa;
+    JTextField Id;
+
+    JLabel LBNume = new JLabel("Nume:");
+    JLabel LBPrenume = new JLabel("Prenume:");
+    JLabel LBSalariu = new JLabel("Salariu:");
+    JLabel LBAdresa = new JLabel("Adresa:");
+    JLabel LBId = new JLabel("Id:");
+    JLabel LBPOza=new JLabel("Poza angajat:");
+
+    JButton ButRefresh=new JButton("Refresh");
+    JButton ButAdauga=new JButton("Adauga");
+    JButton ButSterge=new JButton("Sterge");
+    JButton ButEditeaza=new JButton("Editeaza");
+    JButton ButExport=new JButton("Exporta XML");
+    JButton ButImport=new JButton("Importa XML");
+
+    JTable TableAngajati=new JTable();
+
+
+
     EmployeeController controller;
 
-    public void init(){
-    // initialize layout
+    public void init() {
+        // initialize layout
 
         initForm();
         initButtons();
